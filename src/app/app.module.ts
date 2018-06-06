fe028cb9277475bf3008dbec133794f7992a684a
Talik Kasozi <talik.aziizi@gmail.com>
2018-06-06 04:06:20 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular'; //Footer
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { HomePage, ContentPage1, Introduction, Chapter2, Chapter3 } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ContentPage1,
+    Introduction,
+    Chapter2,
+    Chapter3
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ContentPage1,
+    Introduction,
+    Chapter2,
+    Chapter3  
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+
+@Injectable()
+class BaseComponent implements OnInit{
+  constructor(public navCtrl: NavController){}
+  
+  lastPage() {
+    // Let's navigate from TabsPage to Page1
+    this.navCtrl.pop();
+  }
+  nextPage(){}
+  ngOnInit(){}
+}
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
-export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+export class HomePage extends BaseComponent {
+  lastPage() { this.navCtrl.goToRoot }
+  nextPage() {
+    this.navCtrl.push(ContentPage1)
   }
+  title = "A BookTurner";
+}
 
+@Component({
+  selector: 'content-page',
+  templateUrl:'../content-page.html'
+})
+export class ContentPage1 extends BaseComponent{
+  goTo(choice){
+    switch(choice){
+      case "intro":
+      this.navCtrl.push(Introduction);
+      break;
+      case "chp1":
+      this.navCtrl.push(Chapter2);
+      break;
+      case "chp2":
+      this.navCtrl.push(Chapter3);
+      break;
+      default:
+    }
+  }
+  nextPage() {
+    this.navCtrl.push(Introduction);
+  }
+  title = "Table of Content";
+  page = "I";
+}
+
+@Component({
+  selector:'page1',
+  templateUrl:'../page1.html' 
+})
+export class Introduction extends BaseComponent{
+  nextPage() {
+    this.navCtrl.push(Chapter2);
+  }
+  title = "Introduction";
+  page = "1";
+}
+
+@Component({
+  selector:'page2',
+  templateUrl:'../page2.html' 
+})
+export class Chapter2 extends BaseComponent{
+  nextPage() {
+    this.navCtrl.push(Chapter3);
+  }
+  title = "The Told Story";
+  page = "2";
+}
+
+@Component({
+  selector:'page3',
+  templateUrl:'../page3.html' 
+})
+export class Chapter3 extends BaseComponent{
+  title = "The Last Chapter";
+  page = "2";
 }
